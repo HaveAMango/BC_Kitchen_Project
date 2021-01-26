631c77e1ef78a2d555e7b7eb33feca30334d0d62
Darja Golovina <dagolovinad@gmail.com>
2021-01-26 17:55:58 +0200

    Login form stub

--- app/src/main/java/com/example/bc_kitchen_project/MainActivity.java
+++ app/src/main/java/com/example/bc_kitchen_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bc_kitchen_project;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.bc_kitchen_project.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
