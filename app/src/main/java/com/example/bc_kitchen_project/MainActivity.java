c551b5e235d7070c10a763eec437ad4b3d244650
Darja Golovina <dagolovinad@gmail.com>
2021-01-26 11:14:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bc_kitchen_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bc_kitchen_project;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
