47b603eea8ed5fd4c8ed63c95df65df887180c0a
Zane Kaibe <zane.kaibe@gmail.com>
2021-01-31 18:57:43 +0200

    Feedback implemented - both rating with comment and sending an email. Need to add this to database and display in recycle view.

---
+++ app/src/main/java/com/example/bc_kitchen_project/SendEmail.java
@@ -0,0 +1,52 @@
+package com.example.bc_kitchen_project;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+
+public class SendEmail extends DialogFragment {
+
+    private String selection;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String[] messageType = getActivity().getResources().getStringArray(R.array.messageType);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.chooseMessage);
+        builder.setSingleChoiceItems(R.array.messageType, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selection = messageType[which];
+            }
+        });
+        builder.setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), selection + " message needs to be sent", Toast.LENGTH_SHORT).show();
+                String[] TO = {"zane.kaibe@gmail.com"};
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, TO);
+                email.putExtra(Intent.EXTRA_SUBJECT, selection + " on Pantry item app");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose app to send email"));
+
+            }
+        });
+        builder.setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+
+        return dialog;
+    }
+}
