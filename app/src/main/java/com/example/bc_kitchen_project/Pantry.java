5c7ed9bbe537f26c42b19e964c74ccaf6591237e
Justine Liene Laumane <justinellaumane@gmail.com>
2021-02-03 13:05:12 +0200

    project cleanup and design changes

--- app/src/main/java/com/example/bc_kitchen_project/Pantry.java
+++ app/src/main/java/com/example/bc_kitchen_project/Pantry.java
@@ -35,10 +35,12 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
     private DatabaseReference database;
     private ArrayList<String> productsList = new ArrayList<>();
     ListView theListView;
+    @Override
     public void onBackPressed() {
         Intent intent = new Intent(Pantry.this, HomeActivity.class);
         startActivity(intent);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,28 +51,7 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
         database.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot product : snapshot.getChildren()) {
-                    String name = product.child("name").getValue().toString();
-                    Integer year = Integer.parseInt(product.child("date").child("year").getValue().toString());
-                    Integer month = Integer.parseInt(product.child("date").child("month").getValue().toString());
-                    Integer day = Integer.parseInt(product.child("date").child("date").getValue().toString());
-                    Integer count = Integer.parseInt(product.child("count").getValue().toString());
-                    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                    String dateString;
-                    String old = "";
-                    if (year == 5000) {
-                        productsList.add(name + ",  Count: " + count + " ");
-                    } else {
-                        Date date = new Date(year - 1900, month, day);
-                        dateString = df.format(date);
-                        boolean is = (date).after(new Date());
-                        if (!is) {
-                            old = "BE CAREFUL, IT'S OLD";
-                            productsList.add(name + ",  Count: " + count + " " + old);
-                        } else
-                            productsList.add(name + ",  Expires: " + dateString + " Count: " + count + " ");
-                    }
-                }
+                productsFromDatabaseToList(snapshot);
                 ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Pantry.this,
                         simple_list_item_1,
                         productsList);
@@ -86,27 +67,10 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
                                 switch (which) {
                                     case DialogInterface.BUTTON_POSITIVE:
                                         database = FirebaseDatabase.getInstance().getReference("user-pantry").child(LoginRepository.activeUserId()).child(product);
-                                        database.addListenerForSingleValueEvent(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                                int countCurrent = Integer.parseInt(snapshot.child("count").getValue().toString());
-                                                if (countCurrent == 1) {
-                                                    database.removeValue();
-                                                } else {
-                                                    countCurrent--;
-                                                    database.child("count").setValue(countCurrent);
-                                                }
-                                            }
-
-                                            @Override
-                                            public void onCancelled(@NonNull DatabaseError error) {
-                                                Log.e("products", "error: " + error);
-                                            }
-                                        });
+                                        positiveClick();
                                         dialog.cancel();
                                         askIfAddToGroceryList(product);
                                         break;
-
                                     case DialogInterface.BUTTON_NEGATIVE:
                                         break;
                                 }
@@ -115,8 +79,6 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
                         AlertDialog.Builder builder = new AlertDialog.Builder(Pantry.this);
                         builder.setMessage("Do you want to take one " + product + " out of the pantry?").setPositiveButton("Yes", dialogClickListener)
                                 .setNegativeButton("No", dialogClickListener).show();
-
-
                     }
                 });
             }
@@ -134,11 +96,61 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
             }
         });
     }
-    public void askIfAddToGroceryList(String product){
+
+    private void productsFromDatabaseToList(DataSnapshot snapshot) {
+        for (DataSnapshot product : snapshot.getChildren()) {
+            String name = product.child("name").getValue().toString();
+            Integer year = Integer.parseInt(product.child("date").child("year").getValue().toString());
+            Integer month = Integer.parseInt(product.child("date").child("month").getValue().toString());
+            Integer day = Integer.parseInt(product.child("date").child("date").getValue().toString());
+            Integer count = Integer.parseInt(product.child("count").getValue().toString()); //get products from database
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            String dateString;
+            String old = "";
+            if (year == 5000) {
+                productsList.add(name + ",  Count: " + count + " "); //if date wasn't set, automatically it's 5000
+            } else {
+                Date date = new Date(year - 1900, month, day); //date formating
+                dateString = df.format(date);
+                boolean is = (date).after(new Date()); //checks the date
+                if (!is) {  //warning if old
+                    old = "BE CAREFUL, IT'S OLD";
+                    productsList.add(name + ",  Count: " + count + " " + old);
+                } else
+                    productsList.add(name + ",  Expires: " + dateString + " Count: " + count + " ");
+            }
+        }
+    }
+
+    private void positiveClick() {
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int countCurrent = Integer.parseInt(snapshot.child("count").getValue().toString());
+                removeOneItem(countCurrent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("products", "error: " + error);
+            }
+        });
+    }
+
+    private void removeOneItem(int countCurrent) {
+        if (countCurrent == 1) { //if one product left, deletes it from DB
+            database.removeValue();
+        } else { //if more than one left, rewrites the count to one less
+            countCurrent--;
+            database.child("count").setValue(countCurrent);
+        }
+    }
+
+    public void askIfAddToGroceryList(String product) {
         DialogInterface.OnClickListener dialogClickListener2 = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                switch (which){
+                switch (which) {
                     case DialogInterface.BUTTON_POSITIVE:
                         database = FirebaseDatabase.getInstance().getReference();
                         Product temp = new Product(product);
@@ -158,6 +170,7 @@ public class Pantry extends AppCompatActivity { //very similar to Fridge, look t
         builder2.setMessage("Do you want to add this product to Grocery List?").setPositiveButton("Yes", dialogClickListener2)
                 .setNegativeButton("No", dialogClickListener2).show();
     }
+
     private void writeNewProduct(Product product) { //writes product to database
         database.child("user-groceries").child(LoginRepository.activeUserId()).child(product.name).setValue(product);
     }
